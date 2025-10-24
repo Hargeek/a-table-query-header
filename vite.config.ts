afee4ac9ef54cc9e635103509bc4776bb9edd50c
SSgeek <ssgeek@hotmail.com>
2025-10-24 14:16:23 +0800

    feat: pkg

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import {defineConfig} from 'vite';
+import eslint from 'vite-plugin-eslint';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'AQueryHeader',
-      fileName: 'index',
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['vue', '@arco-design/web-vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          '@arco-design/web-vue': 'ArcoVue',
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            name: 'AQueryHeader',
+            fileName: 'index',
+            formats: ['es', 'cjs'],
         },
-      },
+        rollupOptions: {
+            external: ['vue', '@arco-design/web-vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                    '@arco-design/web-vue': 'ArcoVue',
+                },
+            },
+        },
     },
-  },
-  plugins: [vue(), dts()],
-}); 
+    plugins: [
+        eslint({
+            cache: false,
+            include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+            exclude: ['node_modules'],
+        }),
+        dts(),
+    ],
+});
